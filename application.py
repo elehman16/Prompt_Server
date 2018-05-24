c6c922b7a73a40e45b835b6c4e1aacc3350c6c48
Eric Lehman <lehmer16@gmail.com>
2018-05-24 14:36:54 -0400

    Run online instead of local

--- application.py
+++ application.py
@@ -111,5 +111,5 @@ def results():
 Run the application.
 """
 if __name__ == '__main__':
-    application.run()
-    #application.run(host = '0.0.0.0', port = 8082) 
+    #application.run()
+    application.run(host = '0.0.0.0', port = 8082) 
